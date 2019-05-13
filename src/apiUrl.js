a2aac6b4466466c73c83d001961ddf048af32c27
Ogenbd <ogenbd@gmail.com>
2019-05-13 12:39:08 +0300

    www fix

--- src/apiUrl.js
+++ src/apiUrl.js
@@ -1,7 +1,7 @@
 let apiUrl;
 
 process.env.NODE_ENV === "development"
-  ? (apiUrl = "http://192.168.1.6:3200")
-  : (apiUrl = "https://tennoware.com");
+  ? apiUrl = "http://192.168.1.6:3200"
+  : window.location.host === "www.tennoware.com" ? apiUrl = "https://www.tennoware.com" : apiUrl = "https://tennoware.com";
 
 export default apiUrl;
